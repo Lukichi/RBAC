e41b06219c16df8dafc5fac197b1a17771a822f9
Lukichi <GladBron@mail.ru>
2026-02-14 14:44:21 +0700

    Complete 1.8: create class TemporaryAssignment

--- src/rbac/Main.java
+++ src/rbac/Main.java
@@ -69,7 +69,29 @@ public class Main {
 
 //        ===================================================
 
-        User user = User.validate("test1", "Testor First", "test@mail.ru");
+//        User user = User.validate("test1", "Tester First", "test@mail.ru");
+//
+//        Set<Permission> permissions = new HashSet<>();
+//        permissions.add(new Permission("READ", "testers", "Read users"));
+//        permissions.add(new Permission("ReaD", "testers", "Read users"));
+//        permissions.add(new Permission("Write", "report", "other text"));
+//        permissions.add(new Permission("READ", "tester", "text read"));
+//        Role role = new Role("admin2", "Description for admin", permissions);
+//
+//        AssignmentMetadata metData = AssignmentMetadata.now("ADMIN", "Important reason");
+//
+//        PermanentAssignment assignment = new PermanentAssignment(user, role, metData);
+//        System.out.println(assignment.summary());
+//
+//        PermanentAssignment assignment2 = new PermanentAssignment(user, role, metData);
+//        assignment2.revoke();
+//        System.out.println(assignment2.summary());
+
+//        =======================================================
+
+        DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern("yyyy MM dd HH:mm:ss");
+
+        User user = User.validate("test1", "Tester First", "test@mail.ru");
 
         Set<Permission> permissions = new HashSet<>();
         permissions.add(new Permission("READ", "testers", "Read users"));
@@ -80,11 +102,24 @@ public class Main {
 
         AssignmentMetadata metData = AssignmentMetadata.now("ADMIN", "Important reason");
 
-        PermanentAssignment assignment = new PermanentAssignment(user, role, metData);
+        String date1 = LocalDateTime.now().minusDays(19).format(dataFormat);
+        TemporaryAssignment assignment = new TemporaryAssignment(user, role, metData, date1, false);
         System.out.println(assignment.summary());
 
-        PermanentAssignment assignment2 = new PermanentAssignment(user, role, metData);
-        assignment2.revoke();
+        System.out.println();
+
+        String date2 = LocalDateTime.now().format(dataFormat);
+        TemporaryAssignment assignment2 = new TemporaryAssignment(user, role, metData, date2, true);
         System.out.println(assignment2.summary());
+
+        System.out.println();
+        String date3 = LocalDateTime.now().format(dataFormat);
+        TemporaryAssignment assignment3 = new TemporaryAssignment(user, role, metData, date3, false);
+
+        System.out.println(assignment3.summary());
+//        assignment3.extend(LocalDateTime.now().minusHours(22).format(dataFormat));
+        assignment3.extend(LocalDateTime.now().plusHours(2).format(dataFormat));
+        System.out.println(assignment3.summary());
+
     }
 }
